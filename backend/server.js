9b5c4210b807168e2eb6283c79d51b606d22bd43
SaschaSt <iphone.stumpf@gmail.com>
2025-03-05 12:54:24 +0100

    datenbank erstell, post request bis zu einem gewissen Teil bearbeitet

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,45 @@
 const express = require('express');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
 //TO: verbinde eine Datenbank 
 
-//TODO: schreibe requests/responses
+const db = new sqlite3.Database('./tasks.db');
 
+db.run('CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, completed BOOLEAN DEFAULT 0)');
+
+
+
+app.use(bodyParser.json());         //Middleware
+
+
+//TODO: Schreibe request /response
 
 app.get('/', (req, res) => {
-    res.send('request received');
+    res.send('request');
 });
+
+app.get('/ralf', (req, res) => {
+    res.send('vielen Dank Ralf');
+});
+
+
+
+app.post('/add', (req, res) => {
+    db.run('INSERT INTO tasks (title) VALUES(?)', [req.body.title], function () {;
+        res.json({tag: "Mittwoch", bald_wirds: "Mittagspause"})
+    });
+});
+
+
+
+
+
 
 app.listen(3050, "localhost", () => {
     console.log("bald ist Mittagspause")
-})
+});
 
